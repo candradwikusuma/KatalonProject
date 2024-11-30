e24d7256387916ab214a606281359daa01b6f8a1
candra <candradwikusuma07@gmail.com>
2024-11-30 21:27:09 +0700

    log and report settings

--- Scripts/Demo/manualDemo/Script1732895429070.groovy
+++ Scripts/Demo/manualDemo/Script1732895429070.groovy
@@ -27,7 +27,7 @@ WebUI.setEncryptedText(findTestObject('Page_OrangeHRM/input_Password_password'),
 
 WebUI.click(findTestObject('Page_OrangeHRM/button_Login'))
 
-WebUI.verifyTextPresent('NAVAL LAST', false)
+WebUI.verifyTextPresent('Upgrade', false)
 
 WebUI.closeBrowser()
 
